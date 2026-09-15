3d8c454725b7b0f5b77b058f3401595ff7003e76
WETTENTLLC <WETTENTERTAINMENTLLC@GMAIL.COM>
2026-09-15 16:49:48 -0700

    Restrict Netlify publish files to browser assets

--- netlify-site/js/testNailAide.js
+++
@@ -1,9 +0,0 @@
-const { handleUserQuery } = require('./NailAide');
-
-async function test() {
-    const query = "Tell me about your products";
-    const response = await handleUserQuery(query);
-    console.log("Response:", response);
-}
-
-test();
